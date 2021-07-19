d940b4c13cb761f802397418d35738ef86daa7ec
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-19 18:05:32 -0500

    5.5 - Service - Detalle del producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ProductsComponent } from './products/products.component'
 import { ContactComponent } from './contact/contact.component'
 import { DemoComponent } from './demo/demo.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { ProductDetailComponent } from './product-detail/product-detail.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'demo', component: DemoComponent },
   { path: '**', component: PageNotFoundComponent }
