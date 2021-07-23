d421cff0bc56198936a459cfafcd034c646fc45e
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-22 20:29:20 -0500

    Dashboard schematic

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component'
 import { NavComponent } from './components/nav/nav.component'
 import { TableComponent } from './components/table/table.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 const routes: Routes = [
   { path: '', component: NavComponent, children: [
     { path: 'create', component: ProductFormComponent },
+    { path: '', component: DashboardComponent },
     { path: 'table', component: TableComponent },
   ]},
 ];
