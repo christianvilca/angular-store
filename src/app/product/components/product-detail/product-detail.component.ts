912bfa2fbae60b9b6c6743d09045c8dc3eee4568
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-23 19:05:43 -0500

    7.4.2 DELETE

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -60,5 +60,10 @@ export class ProductDetailComponent implements OnInit {
       console.log(product);
     })
   }
-
+  deleteProduct() {
+    this.productsService.deleteProduct('19')
+    .subscribe( result => {
+      console.log(result);
+    })
+  }
 }
