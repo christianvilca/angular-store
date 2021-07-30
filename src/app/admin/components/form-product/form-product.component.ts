d0b93057c0c0d8cc35ef3dcd420fc77ffc9eb46a
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-30 17:33:54 -0500

    8.3 Estilos Formulario

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from './../../../core/services/products/products.service'
 
 @Component({
   selector: 'app-form-product',
@@ -11,12 +14,27 @@ export class FormProductComponent implements OnInit {
   form!: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) {
     this.buildForm();
   }
 
   ngOnInit(): void {
+  }
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+        .subscribe(result =>{
+          console.log(result)
+          this.router.navigate(['./admin/products'])
+        })
+    }
+    //console.log(this.form.value);
   }
 
   private buildForm() {
