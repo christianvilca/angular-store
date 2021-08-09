4c9ba1978021c2af41a7575c814c5b4d89e9f20f
Christian Vilca <christian.vilcaq@gmail.com>
2021-08-09 13:22:54 -0500

    10.2.1 - Login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from './../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.buildForm();
+  }
+
+  ngOnInit() {
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      this.authService.login(value.email, value.password)
+      .then(() => {
+        this.router.navigate(['/admin']);
+      })
+      .catch(() => {
+        alert('no es valido');
+      });
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
 }
