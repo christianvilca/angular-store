a66d368369363f9fd484efe282c7b9f7efb87ccf
Christian Vilca <christian.vilcaq@gmail.com>
2021-08-10 16:26:38 -0500

    10.3 - Subiendo una imagen a Firebase Storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MyValidators } from './../../../utils/validators';
+import { AngularFireStorage } from '@angular/fire/storage'
+
+import { finalize } from 'rxjs/operators'
 
 import { ProductsService } from './../../../core/services/products/products.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,11 +17,13 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
+  image$!: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
     this.form.valueChanges
@@ -40,6 +46,30 @@ export class FormProductComponent implements OnInit {
         })
     }
     //console.log(this.form.value);
+  }
+
+  uploadFile(event: any) {
+    const file = event.target.files[0]
+    // nombre del archivo
+    const name = 'image.png'
+    const fileRef = this.storage.ref(name)
+    const task = this.storage.upload(name, file)
+
+    // Permite saber cuando finaliza y cuando no
+    task.snapshotChanges()
+      // Como es un observable lo puedo procesar con un pipe
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL()
+          this.image$.subscribe( url => {
+            console.log(url);
+            // se asigna al campo del formulario
+            this.form.get('image')?.setValue(url)
+          })
+        })
+      )
+      // Para que todo se procese
+      .subscribe()
   }
 
   private buildForm() {
